a351d7b22e1a78ed54346333f73dfbc4556fc408
N8boy <jannik.renfordt@ikobb.com>
2023-01-19 13:45:51 +0100

    Added LarvatarServiceProvider.php

---
+++ src/LarvatarServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+class LarvatarServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        //$this->app->make('Renfordt\Larvatar\HasLarvatar');
+    }
+}
