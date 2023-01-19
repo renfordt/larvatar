22ed9bd35577318d31bf7894b06367d6a356cf6d
N8boy <jannik.renfordt@ikobb.com>
2023-01-19 21:40:22 +0100

    Added more tests

--- tests/LarvatarTest.php
+++ tests/LarvatarTest.php
@@ -1,10 +1,11 @@
 <?php declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 use Renfordt\Larvatar\Larvatar;
 
 final class LarvatarTest extends TestCase
 {
-    public function testCheckHexGeneration(): void
+    public function testHexGeneration(): void
     {
         $larvatar = new Larvatar();
         $larvatar->setName('Test Name');
@@ -12,6 +13,26 @@ final class LarvatarTest extends TestCase
         $this->assertEquals(
             '#9c3564',
             $larvatar->generateHexColor()
+        );
+    }
+
+    public function testCreateLarvatarByConstructor(): void
+    {
+        $larvatar = new Larvatar('Test Name');
+        $larvatar->generate();
+        $this->assertEquals(
+            '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564"></circle><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-size: 64">TN</text></svg>',
+            $larvatar->generate()
+        );
+    }
+
+    public function testCreateLarvatarByMethod(): void
+    {
+        $larvatar = new Larvatar();
+        $larvatar->setName('Test Name');
+        $this->assertEquals(
+            '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564"></circle><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-size: 64">TN</text></svg>',
+            $larvatar->generate()
         );
     }
 }
