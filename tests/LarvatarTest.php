8ae997d43aeb5e2299ab2b6cb24bbeeb98418eea
N8boy <jannik.renfordt@ikobb.com>
2023-01-19 21:54:41 +0100

    Removed unneeded method

--- tests/LarvatarTest.php
+++ tests/LarvatarTest.php
@@ -19,7 +19,6 @@ final class LarvatarTest extends TestCase
     public function testCreateLarvatarByConstructor(): void
     {
         $larvatar = new Larvatar('Test Name');
-        $larvatar->generate();
         $this->assertEquals(
             '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564" /><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-size: 64">TN</text></svg>',
             $larvatar->generate()
