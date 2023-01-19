059ef7c432c38029f5ef9c7cd37e87e0d6dfbbd1
N8boy <jannik.renfordt@ikobb.com>
2023-01-19 21:43:00 +0100

    added default value for generate() parameter
    added setFont() method

--- src/Larvatar.php
+++ src/Larvatar.php
@@ -8,18 +8,23 @@ use SVG\SVG;
 
 class Larvatar
 {
-    private int $size = 128;
+    private string $font_path = '';
+    private string $font_family = '';
     private array $names = [];
+    private int $size = 128;
 
-    /*public function GetLarvatar(): string
+    public function generate(array $names = []): string
     {
-
-    }*/
-
-    public function generate(array $names): string
-    {
+        if (empty($names))
+        {
+            $names = $this->names;
+        }
         $larvatar = new SVG($this->size, $this->size);
         $doc = $larvatar->getDocument();
+        if($this->font_path != '' & $this->font_family != '')
+        {
+            SVG::addFont($this->font_path);
+        }
         $half_size = $this->size / 2;
 
         $circle = new SVGCircle($half_size, $half_size, $half_size);
@@ -34,8 +39,8 @@ class Larvatar
         $initials_SVG->setStyle('fill', '#ffffff');
         $initials_SVG->setStyle('text-anchor', 'middle');
         $initials_SVG->setStyle('dominant-baseline', 'middle');
-        //$initials_SVG->setFontFamily('Roboto');
-        $initials_SVG->setFontSize($this->size * 0.6);
+        $initials_SVG->setFontFamily($this->font_family);
+        $initials_SVG->setFontSize($this->size * 0.5);
 
         $doc->addChild($circle);
         $doc->addChild($initials_SVG);
@@ -51,6 +56,12 @@ class Larvatar
     public function setName(string $name): void
     {
         $this->names = explode(' ', $name);
+    }
+
+    public function setFont(string $font_family, string $path): void
+    {
+        $this->font_family = $font_family;
+        $this->font_path = $path;
     }
 
     public function generateHexColor(array $names = null, $offset = 0): string
