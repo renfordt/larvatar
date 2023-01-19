d9fccea8bc62244595d5fb060b94dff40636dc34
N8boy <jannik.renfordt@ikobb.com>
2023-01-19 11:04:38 +0100

    Fixed test and removed phpcs

--- tests/GravatarTest.php
+++ tests/GravatarTest.php
@@ -7,7 +7,7 @@ final class GravatarTest extends TestCase
     public function testCanBeUsedAsString(): void
     {
         $this->assertEquals(
-            'b58996c504c5638798eb6b511e6f49af',
+            'http://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af',
             Gravatar::GetGravatarLink('user@example.com')
         );
     }
