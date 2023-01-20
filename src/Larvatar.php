0bcdb08b203be53af28b3ca1d3954a1415347cc2
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 20:45:58 +0100

    Added Larvatar class

--- src/Larvatar.php
+++ src/Larvatar.php
@@ -2,79 +2,35 @@
 
 namespace Renfordt\Larvatar;
 
-use SVG\Nodes\Shapes\SVGCircle;
-use SVG\Nodes\Texts\SVGText;
-use SVG\SVG;
+use Renfordt\Larvatar\Enum\LarvatarTypes;
 
 class Larvatar
 {
-    private string $font_path = '';
-    private string $font_family = '';
-    private array $names = [];
-    private int $size = 128;
+    protected LarvatarTypes $type = LarvatarTypes::mp;
+    protected string $name;
+    protected string $email;
 
-    public function __construct(string $name = '')
+    public function getImage()
     {
-        $this->setName($name);
-    }
-
-    public function generate(array $names = []): string
-    {
-        if (empty($names)) {
-            $names = $this->names;
-        }
-        $larvatar = new SVG($this->size, $this->size);
-        $doc = $larvatar->getDocument();
-        if ($this->font_path != '' & $this->font_family != '') {
-            SVG::addFont($this->font_path);
-        }
-        $half_size = $this->size / 2;
-
-        $circle = new SVGCircle($half_size, $half_size, $half_size);
-        $circle->setStyle('fill', $this->generateHexColor($names));
-
-        $initials = '';
-        foreach ($names as $name) {
-            $initials .= substr($name, 0, 1);
+        if ($this->type == LarvatarTypes::InitialsAvatar) {
+            $initial_avatar = new InitialsAvatar($this->name);
+            return $initial_avatar->generate();
         }
 
-        $initials_SVG = new SVGText($initials, '50%', '55%');
-        $initials_SVG->setStyle('fill', '#ffffff');
-        $initials_SVG->setStyle('text-anchor', 'middle');
-        $initials_SVG->setStyle('dominant-baseline', 'middle');
-        $initials_SVG->setFontFamily($this->font_family);
-        $initials_SVG->setFontSize($this->size * 0.5);
-
-        $doc->addChild($circle);
-        $doc->addChild($initials_SVG);
-
-        return $larvatar;
+        $gravatar = new Gravatar($this->email);
+        $gravatar->setType($this->type);
+        return $gravatar->generateGravatarLink();
     }
 
-    public function setSize(int $size): void
+    public function __construct(string $name, string $email, int|LarvatarTypes $type)
     {
-        $this->size = $size;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->names = explode(' ', $name);
-    }
-
-    public function setFont(string $font_family, string $path): void
-    {
-        $this->font_family = $font_family;
-        $this->font_path = $path;
-    }
-
-    public function generateHexColor(array $names = null, $offset = 0): string
-    {
-        if ($names == null) {
-            $names = $this->names;
+        $this->name = $name;
+        $this->email = $email;
+        if (is_int($type)) {
+            $this->type = LarvatarTypes::from($type);
+        } elseif ($type instanceof LarvatarTypes) {
+            $this->type = $type;
         }
-        $name = implode(' ', $names);
-        $hash = md5($name);
-        return '#'.substr($hash, $offset, 6);
     }
 
 }
