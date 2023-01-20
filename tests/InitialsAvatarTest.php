b57e846b6d053915deb84e0ad6c78e758d7f5f97
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 22:35:12 +0100

    Added more tests for InitialsAvatar

--- tests/InitialsAvatarTest.php
+++ tests/InitialsAvatarTest.php
@@ -34,4 +34,26 @@ final class InitialsAvatarTest extends TestCase
             $initialsAvatar->generate()
         );
     }
+
+    public function testSetFont(): void
+    {
+        $initialsAvatar = new InitialsAvatar();
+        $initialsAvatar->setName('Test Name');
+        $initialsAvatar->setFont('Roboto', './font/Roboto-Bold.ttf');
+
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564" /><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-family: Roboto; font-size: 64">TN</text></svg>',
+            $initialsAvatar->generate()
+        );
+    }
+
+    public function testSetSize(): void
+    {
+        $initialsAvatar = new InitialsAvatar('Test Name');
+        $initialsAvatar->setSize(500);
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="500"><circle cx="250" cy="250" r="250" style="fill: #9c3564" /><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-size: 250">TN</text></svg>',
+            $initialsAvatar->generate()
+        );
+    }
 }
