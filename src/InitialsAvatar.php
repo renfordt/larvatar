30fd317f247def8d7961cfeb395af7f2bc388081
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 17:11:45 +0100

    Renamed Larvatar to InitialsAvatar

---
+++ src/InitialsAvatar.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Renfordt\Larvatar;
+
+use SVG\Nodes\Shapes\SVGCircle;
+use SVG\Nodes\Texts\SVGText;
+use SVG\SVG;
+
+class InitialsAvatar
+{
+    private string $font_path = '';
+    private string $font_family = '';
+    private array $names = [];
+    private int $size = 128;
+
+    public function __construct(string $name = '')
+    {
+        $this->setName($name);
+    }
+
+    public function generate(array $names = []): string
+    {
+        if (empty($names)) {
+            $names = $this->names;
+        }
+        $larvatar = new SVG($this->size, $this->size);
+        $doc = $larvatar->getDocument();
+        if ($this->font_path != '' & $this->font_family != '') {
+            SVG::addFont($this->font_path);
+        }
+        $half_size = $this->size / 2;
+
+        $circle = new SVGCircle($half_size, $half_size, $half_size);
+        $circle->setStyle('fill', $this->generateHexColor($names));
+
+        $initials = '';
+        foreach ($names as $name) {
+            $initials .= substr($name, 0, 1);
+        }
+
+        $initials_SVG = new SVGText($initials, '50%', '55%');
+        $initials_SVG->setStyle('fill', '#ffffff');
+        $initials_SVG->setStyle('text-anchor', 'middle');
+        $initials_SVG->setStyle('dominant-baseline', 'middle');
+        $initials_SVG->setFontFamily($this->font_family);
+        $initials_SVG->setFontSize($this->size * 0.5);
+
+        $doc->addChild($circle);
+        $doc->addChild($initials_SVG);
+
+        return $larvatar;
+    }
+
+    public function setSize(int $size): void
+    {
+        $this->size = $size;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->names = explode(' ', $name);
+    }
+
+    public function setFont(string $font_family, string $path): void
+    {
+        $this->font_family = $font_family;
+        $this->font_path = $path;
+    }
+
+    public function generateHexColor(array $names = null, $offset = 0): string
+    {
+        if ($names == null) {
+            $names = $this->names;
+        }
+        $name = implode(' ', $names);
+        $hash = md5($name);
+        return '#'.substr($hash, $offset, 6);
+    }
+
+}
--- tests/InitialsAvatarTest.php
+++ tests/InitialsAvatarTest.php
@@ -1,37 +1,37 @@
 <?php declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use Renfordt\Larvatar\Larvatar;
+use Renfordt\Larvatar\InitialsAvatar;
 
-final class LarvatarTest extends TestCase
+final class InitialsAvatarTest extends TestCase
 {
     public function testHexGeneration(): void
     {
-        $larvatar = new Larvatar();
-        $larvatar->setName('Test Name');
+        $initialsAvatar = new InitialsAvatar();
+        $initialsAvatar->setName('Test Name');
 
         $this->assertEquals(
             '#9c3564',
-            $larvatar->generateHexColor()
+            $initialsAvatar->generateHexColor()
         );
     }
 
     public function testCreateLarvatarByConstructor(): void
     {
-        $larvatar = new Larvatar('Test Name');
+        $initialsAvatar = new InitialsAvatar('Test Name');
         $this->assertEquals(
             '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564" /><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-size: 64">TN</text></svg>',
-            $larvatar->generate()
+            $initialsAvatar->generate()
         );
     }
 
     public function testCreateLarvatarByMethod(): void
     {
-        $larvatar = new Larvatar();
-        $larvatar->setName('Test Name');
+        $initialsAvatar = new InitialsAvatar();
+        $initialsAvatar->setName('Test Name');
         $this->assertEquals(
             '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564" /><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-size: 64">TN</text></svg>',
-            $larvatar->generate()
+            $initialsAvatar->generate()
         );
     }
 }
