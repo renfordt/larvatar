d2c1716b59ec547148164e2410d85907181f3d6d
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 21:35:56 +0100

    Added more tests for Gravatar

--- tests/GravatarTest.php
+++ tests/GravatarTest.php
@@ -23,4 +23,54 @@ final class GravatarTest extends TestCase
             $gravatar->generateGravatarLink()
         );
     }
+
+    public function testGravatarIdenticonLink(): void
+    {
+        $gravatar = new Gravatar('user@example.com');
+        $gravatar->setType(LarvatarTypes::identicon);
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?d=identicon&f=y',
+            $gravatar->generateGravatarLink()
+        );
+    }
+
+    public function testGravatarMonsteridLink(): void
+    {
+        $gravatar = new Gravatar('user@example.com');
+        $gravatar->setType(LarvatarTypes::monsterid);
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?d=monsterid&f=y',
+            $gravatar->generateGravatarLink()
+        );
+    }
+
+    public function testGravatarWavatarLink(): void
+    {
+        $gravatar = new Gravatar('user@example.com');
+        $gravatar->setType(LarvatarTypes::wavatar);
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?d=wavatar&f=y',
+            $gravatar->generateGravatarLink()
+        );
+    }
+
+    public function testGravatarRetroLink(): void
+    {
+        $gravatar = new Gravatar('user@example.com');
+        $gravatar->setType(LarvatarTypes::retro);
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?d=retro&f=y',
+            $gravatar->generateGravatarLink()
+        );
+    }
+
+    public function testGravatarRobohashLink(): void
+    {
+        $gravatar = new Gravatar('user@example.com');
+        $gravatar->setType(LarvatarTypes::robohash);
+        $this->assertEquals(
+            'https://www.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?d=robohash&f=y',
+            $gravatar->generateGravatarLink()
+        );
+    }
 }
