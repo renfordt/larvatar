34abd737f74c0b325cbdd17f7aa9fa6eaa0aabf3
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 17:14:25 +0100

    Added Enum LarvatarTypes

---
+++ src/Enum/LarvatarTypes.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Renfordt\Larvatar\Enum;
+
+enum LarvatarTypes: int
+{
+    case InitialsAvatar = 0;
+    case Gravatar = 1;
+    case mp = 2;
+    case identicon = 3;
+    case monsterid = 4;
+    case wavatar = 5;
+    case retro = 6;
+    case robohash = 7;
+}
