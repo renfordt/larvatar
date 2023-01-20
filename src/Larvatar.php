2a5cb90e046cb289a2e2c86b741e40ed10d4d146
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 23:44:06 +0100

    Refactoring and added setFont to Larvatar

--- src/Larvatar.php
+++ src/Larvatar.php
@@ -9,18 +9,8 @@ class Larvatar
     protected LarvatarTypes $type = LarvatarTypes::mp;
     protected string $name;
     protected string $email;
-
-    public function getImageHTML()
-    {
-        if ($this->type == LarvatarTypes::InitialsAvatar) {
-            $initial_avatar = new InitialsAvatar($this->name);
-            return $initial_avatar->generate();
-        }
-
-        $gravatar = new Gravatar($this->email);
-        $gravatar->setType($this->type);
-        return '<img src="'.$gravatar->generateGravatarLink().'" />';
-    }
+    protected string $font;
+    protected string $font_path;
 
     public function __construct(string $name, string $email, int|LarvatarTypes $type)
     {
@@ -31,6 +21,27 @@ class Larvatar
         } elseif ($type instanceof LarvatarTypes) {
             $this->type = $type;
         }
+    }
+
+    public function getImageHTML()
+    {
+        if ($this->type == LarvatarTypes::InitialsAvatar) {
+            $initial_avatar = new InitialsAvatar($this->name);
+            if ($this->font != '' & $this->font_path != '') {
+                $initial_avatar->setFont($this->font, $this->font_path);
+            }
+            return $initial_avatar->generate();
+        }
+
+        $gravatar = new Gravatar($this->email);
+        $gravatar->setType($this->type);
+        return '<img src="'.$gravatar->generateGravatarLink().'" />';
+    }
+
+    public function setFont(string $font_family, string $path): void
+    {
+        $this->font_family = $font_family;
+        $this->font_path = $path;
     }
 
 }
