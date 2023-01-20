e6a99cd214babf53b9876c10c8ef81d04e09b02f
N8boy <jannik.renfordt@ikobb.com>
2023-01-20 23:50:10 +0100

    Added SetFont test to Larvatar

--- tests/LarvatarTest.php
+++ tests/LarvatarTest.php
@@ -15,6 +15,16 @@ class LarvatarTest extends TestCase
         );
     }
 
+    public function testSetFont(): void
+    {
+        $larvatar = new Larvatar('Test Name', 'test@example.com', LarvatarTypes::InitialsAvatar);
+        $larvatar->setFont('Roboto', __DIR__.'/../src/font/Roboto-Bold.ttf');
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="128" height="128"><circle cx="64" cy="64" r="64" style="fill: #9c3564" /><text x="50%" y="55%" style="fill: #ffffff; text-anchor: middle; dominant-baseline: middle; font-family: Roboto; font-size: 64">TN</text></svg>',
+            $larvatar->getImageHTML()
+        );
+    }
+
     public function testCreateGravatar(): void
     {
         $larvatar = new Larvatar('Test Name', 'test@example.com', LarvatarTypes::Gravatar);
