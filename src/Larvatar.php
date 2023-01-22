633f5936b4309b9a6e199902600716e57ecf29f3
N8boy <jannik.renfordt@ikobb.com>
2023-01-22 19:10:18 +0100

    Reduced complexity and added default size

--- src/Larvatar.php
+++ src/Larvatar.php
@@ -11,7 +11,7 @@ class Larvatar
     protected string $email;
     protected string $font;
     protected string $fontPath;
-    protected int $size;
+    protected int $size = 100;
 
     public function __construct(string $name = '', string $email = '', int|LarvatarTypes $type = LarvatarTypes::mp)
     {
@@ -24,24 +24,21 @@ class Larvatar
         }
     }
 
-    public function getImageHTML()
+    public function getImageHTML(): string
     {
         if ($this->type == LarvatarTypes::InitialsAvatar) {
             $initial_avatar = new InitialsAvatar($this->name);
             if (isset($this->font) && $this->font != '' && $this->fontPath != '') {
                 $initial_avatar->setFont($this->font, $this->fontPath);
             }
-            if (isset($this->size)) {
-                $initial_avatar->setSize($this->size);
-            }
+            $initial_avatar->setSize($this->size);
             return $initial_avatar->generate();
         }
 
         $gravatar = new Gravatar($this->email);
         $gravatar->setType($this->type);
-        if (isset($this->size)) {
-            $gravatar->setSize($this->size);
-        }
+        $gravatar->setSize($this->size);
+
         return '<img src="'.$gravatar->generateGravatarLink().'" />';
     }
 
