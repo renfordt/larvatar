b7960ee74aba34d6f03e1684d6d5b307c1e49bc3
Jannik Renfordt <jannikrenfordt@gmx.de>
2024-04-01 00:29:07 +0200

    Refactor Color.php for better readability and logic
    The Color.php file was refactored to improve readability and streamline its logic. Color calculations are now separated into distinct methods and redundant code was eliminated. The changes also include reordering class methods for consistent access modifiers.

--- src/Color.php
+++ src/Color.php
@@ -84,91 +84,6 @@ class Color
     }
 
     /**
-     * Convert RGB color to HSV color space.
-     *
-     * @param  int  $red  The red component of the RGB color (0-255).
-     * @param  int  $green  The green component of the RGB color (0-255).
-     * @param  int  $blue  The blue component of the RGB color (0-255).
-     * @return array An array containing the HSV color values (hue, saturation, value).
-     */
-    public static function RGBToHSV(int $red, int $green, int $blue): array
-    {
-        list($maxRGB, $minRGB, $chroma, $value, $hue) = self::calculateCVH($red, $green, $blue);
-
-        if ($chroma == 0) {
-            return array(0, 0, $value);
-        }
-
-        $saturation = $chroma / $maxRGB * 100;
-
-
-        return array($hue, $saturation, $value);
-    }
-
-    /**
-     * Convert HSV color to RGB color space.
-     *
-     * @param  int  $hue  The hue component of the HSV color (0-360).
-     * @param  float  $saturation  The saturation component of the HSV color (0-1).
-     * @param  float  $value  The value component of the HSV color (0-1).
-     * @return array An array containing the RGB color values (red, green, blue).
-     * @throws InvalidArgumentException if any of the parameters exceed their intended ranges.
-     * @throws Exception if RGB calculation is not possible.
-     */
-    public static function HSVToRGB(int $hue, float $saturation, float $value): array
-    {
-        if ($hue < 0 || $hue > 360 ||
-            $saturation < 0 || $saturation > 1 ||
-            $value < 0 || $value > 1) {
-            throw new InvalidArgumentException('Parameters exceed their intended ranges.');
-        }
-
-        $chroma = $value * $saturation;
-        $hueNormalized = $hue / 60;
-        $hMod2 = $hueNormalized - 2 * floor($hueNormalized / 2);
-        $secondMax = $chroma * (1 - abs($hMod2 - 1));
-
-        if (0 <= $hueNormalized && $hueNormalized < 1) {
-            list($r, $g, $b) = array($chroma, $secondMax, 0);
-        } elseif (1 <= $hueNormalized && $hueNormalized < 2) {
-            list($r, $g, $b) = array($secondMax, $chroma, 0);
-        } elseif (2 <= $hueNormalized && $hueNormalized < 3) {
-            list($r, $g, $b) = array(0, $chroma, $secondMax);
-        } elseif (3 <= $hueNormalized && $hueNormalized < 4) {
-            list($r, $g, $b) = array(0, $secondMax, $chroma);
-        } elseif (4 <= $hueNormalized && $hueNormalized < 5) {
-            list($r, $g, $b) = array($secondMax, 0, $chroma);
-        } elseif (5 <= $hueNormalized && $hueNormalized < 6) {
-            list($r, $g, $b) = array($chroma, 0, $secondMax);
-        }
-        if (!isset($r) || !isset($g) || !isset($b)) {
-            throw new Exception('RGB calculation not possible. Check inputs!');
-        }
-        $m = $value - $chroma;
-        $r = intval(round(($r + $m) * 255));
-        $g = intval(round(($g + $m) * 255));
-        $b = intval(round(($b + $m) * 255));
-
-        return array($r, $g, $b);
-    }
-
-    /**
-     * Convert RGB color to hexadecimal color representation.
-     *
-     * @param  int  $red  The red component of the RGB color (0-255).
-     * @param  int  $green  The green component of the RGB color (0-255).
-     * @param  int  $blue  The blue component of the RGB color (0-255).
-     * @return string The hexadecimal representation of the RGB color.
-     */
-    public static function RGBToHex(int $red, int $green, int $blue): string
-    {
-        $hexr = str_pad(dechex($red), 2, "0", STR_PAD_LEFT);
-        $hexg = str_pad(dechex($green), 2, "0", STR_PAD_LEFT);
-        $hexb = str_pad(dechex($blue), 2, "0", STR_PAD_LEFT);
-        return $hexr.$hexg.$hexb;
-    }
-
-    /**
      * Calculate the components Chroma, Value, and Hue based on RGB color.
      *
      * @param  int  $red  The red component of the RGB color (0-255).
@@ -203,6 +118,100 @@ class Color
     }
 
     /**
+     * Convert RGB color to HSV color space.
+     *
+     * @param  int  $red  The red component of the RGB color (0-255).
+     * @param  int  $green  The green component of the RGB color (0-255).
+     * @param  int  $blue  The blue component of the RGB color (0-255).
+     * @return array An array containing the HSV color values (hue, saturation, value).
+     */
+    public static function RGBToHSV(int $red, int $green, int $blue): array
+    {
+        list($maxRGB, $minRGB, $chroma, $value, $hue) = self::calculateCVH($red, $green, $blue);
+
+        if ($chroma == 0) {
+            return array(0, 0, $value);
+        }
+
+        $saturation = $chroma / $maxRGB * 100;
+
+
+        return array($hue, $saturation, $value);
+    }
+
+    /**
+     * Convert HSV color to RGB color space.
+     *
+     * @param  int  $hue  The hue component of the HSV color (0-360).
+     * @param  float  $saturation  The saturation component of the HSV color (0-1).
+     * @param  float  $value  The value component of the HSV color (0-1).
+     * @return array An array containing the RGB color values (red, green, blue).
+     * @throws InvalidArgumentException if any of the parameters exceed their intended ranges.
+     * @throws Exception if RGB calculation is not possible.
+     */
+    public static function HSVToRGB(int $hue, float $saturation, float $value): array
+    {
+        self::validateParameters($hue, $saturation, $value);
+
+        $chroma = $value * $saturation;
+        $hueNormalized = $hue / 60;
+        $hMod2 = $hueNormalized - 2 * floor($hueNormalized / 2);
+        $secondMax = $chroma * (1 - abs($hMod2 - 1));
+
+        list($r, $g, $b) = self::calculateRGBRange($hueNormalized, $chroma, $secondMax);
+
+        return self::finalizeRGBCalculation($r, $g, $b, $value, $chroma);
+    }
+
+    private static function validateParameters(int $hue, float $saturation, float $value): void
+    {
+        if ($hue < 0 || $hue > 360 ||
+            $saturation < 0 || $saturation > 1 ||
+            $value < 0 || $value > 1) {
+            throw new InvalidArgumentException('Parameters exceed their intended ranges.');
+        }
+    }
+
+    private static function calculateRGBRange(float $hueNormalized, float $chroma, float $secondMax): array
+    {
+        if (0 <= $hueNormalized && $hueNormalized < 1) {
+            return [$chroma, $secondMax, 0];
+        } elseif (1 <= $hueNormalized && $hueNormalized < 2) {
+            return [$secondMax, $chroma, 0];
+        } elseif (2 <= $hueNormalized && $hueNormalized < 3) {
+            return [0, $chroma, $secondMax];
+        } elseif (3 <= $hueNormalized && $hueNormalized < 4) {
+            return [0, $secondMax, $chroma];
+        } elseif (4 <= $hueNormalized && $hueNormalized < 5) {
+            return [$secondMax, 0, $chroma];
+        } else {
+            return [$chroma, 0, $secondMax];
+        }
+    }
+
+    private static function finalizeRGBCalculation(float $r, float $g, float $b, float $value, float $chroma): array
+    {
+        $m = $value - $chroma;
+        return array_map(fn($x) => intval(round(($x + $m) * 255)), [$r, $g, $b]);
+    }
+
+    /**
+     * Convert RGB color to hexadecimal color representation.
+     *
+     * @param  int  $red  The red component of the RGB color (0-255).
+     * @param  int  $green  The green component of the RGB color (0-255).
+     * @param  int  $blue  The blue component of the RGB color (0-255).
+     * @return string The hexadecimal representation of the RGB color.
+     */
+    public static function RGBToHex(int $red, int $green, int $blue): string
+    {
+        $hexr = str_pad(dechex($red), 2, "0", STR_PAD_LEFT);
+        $hexg = str_pad(dechex($green), 2, "0", STR_PAD_LEFT);
+        $hexb = str_pad(dechex($blue), 2, "0", STR_PAD_LEFT);
+        return $hexr.$hexg.$hexb;
+    }
+
+    /**
      * Convert HSL color to RGB color space.
      *
      * @param  int  $hue  The hue component of the HSL color (0-359).
@@ -218,28 +227,65 @@ class Color
         $hMod2 = $hueNormalized - 2 * floor($hueNormalized / 2);
         $intermediateValue = $chroma * (1 - abs($hMod2 - 1));
 
-        if (0 <= $hueNormalized && $hueNormalized < 1) {
-            list($r, $g, $b) = array($chroma, $intermediateValue, 0);
-        } elseif (1 <= $hueNormalized && $hueNormalized < 2) {
-            list($r, $g, $b) = array($intermediateValue, $chroma, 0);
-        } elseif (2 <= $hueNormalized && $hueNormalized < 3) {
-            list($r, $g, $b) = array(0, $chroma, $intermediateValue);
-        } elseif (3 <= $hueNormalized && $hueNormalized < 4) {
-            list($r, $g, $b) = array(0, $intermediateValue, $chroma);
-        } elseif (4 <= $hueNormalized && $hueNormalized < 5) {
-            list($r, $g, $b) = array($intermediateValue, 0, $chroma);
-        } elseif (5 <= $hueNormalized && $hueNormalized < 6) {
-            list($r, $g, $b) = array($chroma, 0, $intermediateValue);
-        }
-        if (!isset($r) || !isset($g) || !isset($b)) {
-            throw new Exception('RGB calculation not possible. Check inputs!');
-        }
-        $m = $lightness - $chroma / 2;
-        $r = intval(round(($r + $m) * 255));
-        $g = intval(round(($g + $m) * 255));
-        $b = intval(round(($b + $m) * 255));
+        list($r, $g, $b) = self::calculateRGBRange($hueNormalized, $chroma, $intermediateValue);
 
-        return array($r, $g, $b);
+        return self::finalizeRGBCalculation($r, $g, $b, $lightness, $chroma);
+    }
+
+    /**
+     * Get the HSL color values of the current object.
+     *
+     * @return array An array containing the HSL color values (hue, saturation, lightness).
+     */
+    public function getHSL(): array
+    {
+        return $this->hsl;
+    }
+
+    /**
+     * Set the HSL color values and update the RGB and hex color values.
+     *
+     * @param  array  $color  An array containing the HSL color values (hue, saturation, lightness).
+     * @return void
+     */
+    public function setHSL(array $color): void
+    {
+        $this->hsl = $color;
+        $this->rgb = $this->HSLToRGB($color[0], $color[1], $color[2]);
+        $this->hex = $this->RGBToHex($this->rgb[0], $this->rgb[1], $this->rgb[2]);
+    }
+
+    /**
+     * Get the RGB color values.
+     *
+     * @return array An array containing the RGB color values (red, green, blue).
+     */
+    public function getRGB(): array
+    {
+        return $this->rgb;
+    }
+
+    /**
+     * Set the RGB color.
+     *
+     * @param  array  $color  An array containing the RGB color values (red, green, blue).
+     * @return void
+     */
+    public function setRGB(array $color): void
+    {
+        $this->rgb = $color;
+        $this->hex = Color::RGBToHex($color[0], $color[1], $color[2]);
+        $this->hsl = Color::RGBToHSL($color[0], $color[1], $color[2]);
+    }
+
+    /**
+     * Get the hexadecimal representation of the color.
+     *
+     * @return string The hexadecimal string representation of the color.
+     */
+    public function getHex(): string
+    {
+        return '#'.$this->hex;
     }
 
     /**
@@ -259,62 +305,6 @@ class Color
 
         $this->rgb = Color::HexToRGB($this->hex);
         $this->hsl = Color::RGBToHSL($this->rgb[0], $this->rgb[1], $this->rgb[2]);
-    }
-
-    /**
-     * Set the RGB color.
-     *
-     * @param  array  $color  An array containing the RGB color values (red, green, blue).
-     * @return void
-     */
-    public function setRGB(array $color): void
-    {
-        $this->rgb = $color;
-        $this->hex = Color::RGBToHex($color[0], $color[1], $color[2]);
-        $this->hsl = Color::RGBToHSL($color[0], $color[1], $color[2]);
-    }
-
-    /**
-     * Set the HSL color values and update the RGB and hex color values.
-     *
-     * @param  array  $color  An array containing the HSL color values (hue, saturation, lightness).
-     * @return void
-     */
-    public function setHSL(array $color): void
-    {
-        $this->hsl = $color;
-        $this->rgb = $this->HSLToRGB($color[0], $color[1], $color[2]);
-        $this->hex = $this->RGBToHex($this->rgb[0], $this->rgb[1], $this->rgb[2]);
-    }
-
-    /**
-     * Get the HSL color values of the current object.
-     *
-     * @return array An array containing the HSL color values (hue, saturation, lightness).
-     */
-    public function getHSL(): array
-    {
-        return $this->hsl;
-    }
-
-    /**
-     * Get the RGB color values.
-     *
-     * @return array An array containing the RGB color values (red, green, blue).
-     */
-    public function getRGB(): array
-    {
-        return $this->rgb;
-    }
-
-    /**
-     * Get the hexadecimal representation of the color.
-     *
-     * @return string The hexadecimal string representation of the color.
-     */
-    public function getHex(): string
-    {
-        return '#'.$this->hex;
     }
 
     /**
@@ -351,19 +341,6 @@ class Color
     }
 
     /**
-     * Darken the color by reducing its lightness value.
-     *
-     * @param  int  $amount  The amount by which to darken the color (0-100).
-     * @return void
-     */
-    public function darken(int $amount = 10): void
-    {
-        list($hue, $saturation, $lightness) = $this->hsl;
-        $lightness = self::clamp($lightness - $amount / 100, 0, 1);
-        $this->setHSL(array($hue, $saturation, $lightness));
-    }
-
-    /**
      * Clamp a number between a minimum and maximum value.
      *
      * @param  int|float  $num  The number to clamp.
@@ -379,6 +356,19 @@ class Color
             $num = $min;
         }
         return $num;
+    }
+
+    /**
+     * Darken the color by reducing its lightness value.
+     *
+     * @param  int  $amount  The amount by which to darken the color (0-100).
+     * @return void
+     */
+    public function darken(int $amount = 10): void
+    {
+        list($hue, $saturation, $lightness) = $this->hsl;
+        $lightness = self::clamp($lightness - $amount / 100, 0, 1);
+        $this->setHSL(array($hue, $saturation, $lightness));
     }
 
 }
