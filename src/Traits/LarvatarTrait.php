368ad0f16b56410046222b128e28b30d43340163
Jannik Renfordt <jannikrenfordt@gmx.de>
2024-04-11 21:19:06 +0200

    Add LarvatarTrait for avatar image retrieval
    A new trait, LarvatarTrait, has been introduced. This contains the getAvatar method, allowing for the retrieving of avatar images based on provided name, email, avatar type, and encoding. This helps in streamlining the avatar image generation process within the application.

---
+++ src/Traits/LarvatarTrait.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Renfordt\Larvatar\Traits;
+
+use Renfordt\Larvatar\Enum\LarvatarTypes;
+use Renfordt\Larvatar\Larvatar;
+
+trait LarvatarTrait
+{
+    /**
+     * Retrieves the avatar image HTML based on the provided name, email, avatar type, and encoding.
+     *
+     * @param  string  $name  The name used to generate the avatar.
+     * @param  string  $email  The email address used to generate the avatar.
+     * @param  LarvatarTypes  $type  The type of avatar to generate. Default is initial avatars.
+     * @param  string  $encoding  The encoding type for the avatar image. Default is empty string.
+     * @return string The HTML representation of the avatar image.
+     */
+    public function getAvatar(string $name, string $email = '', LarvatarTypes $type = LarvatarTypes::InitialsAvatar, string $encoding = ''): string
+    {
+        $larvatar = new Larvatar($name, $email, $type);
+        return $larvatar->getImageHTML($encoding);
+    }
+}
