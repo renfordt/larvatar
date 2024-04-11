a7cfaa3c8ce854773ae78afbc8204bc0dadf9285
Jannik Renfordt <jannikrenfordt@gmx.de>
2024-04-11 21:53:25 +0200

    Update testGetAvatar to include size parameter
    The test method 'testGetAvatar' in the LarvatarTraitTest class has been updated to include 'size' as an argument. This aligns with any changes made in the corresponding 'getAvatar' method. Modifications also reflect in the data provider for this test.

--- tests/Traits/LarvatarTraitTest.php
+++ tests/Traits/LarvatarTraitTest.php
@@ -14,20 +14,25 @@ class LarvatarTraitTest extends TestCase
     /**
      * @dataProvider dataProviderForGetAvatarTest
      */
-    public function testGetAvatar(string $name, string $email, LarvatarTypes $type, string $encoding = '', string $expectedData)
-    {
-        $result = $this->getAvatar($name, $email, $type, $encoding);
+    public function testGetAvatar(
+        string $name,
+        string $email,
+        int $size,
+        LarvatarTypes $type,
+        string $encoding = '',
+        string $expectedData
+    ) {
+        $result = $this->getAvatar($name, $email, $size, $type, $encoding);
         $this->assertSame($expectedData, $result);
     }
 
     public function dataProviderForGetAvatarTest(): array
     {
         return [
-            // Add test data here as per the format
-            // ['Name', 'Email', LarvatarTypes::<type>, 'encoding', 'expectedData']
             [
                 'Test Name',
                 'test@test.com',
+                100,
                 LarvatarTypes::InitialsAvatar,
                 'base64',
                 '<img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI1MCIgc3R5bGU9ImZpbGw6ICNlNWIzY2EiIC8+PHRleHQgeD0iNTAlIiB5PSI1NSUiIHN0eWxlPSJmaWxsOiAjODUyZTU1OyB0ZXh0LWFuY2hvcjogbWlkZGxlOyBkb21pbmFudC1iYXNlbGluZTogbWlkZGxlOyBmb250LXNpemU6IDUwcHgiPlROPC90ZXh0Pjwvc3ZnPg==" />'
