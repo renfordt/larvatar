4e1987836cacd4869eda86179b82dd797fa44b5e
Jannik Renfordt <jannikrenfordt@gmx.de>
2024-04-11 21:21:23 +0200

    Update image data in LarvatarTraitTest
    The source data for the image in LarvatarTraitTest has been updated. The old JPEG base64 data string has been replaced with a new SVG base64 data string to suit updated requirements and improve test accuracy.

--- tests/Traits/LarvatarTraitTest.php
+++ tests/Traits/LarvatarTraitTest.php
@@ -30,7 +30,7 @@ class LarvatarTraitTest extends TestCase
                 'test@test.com',
                 LarvatarTypes::InitialsAvatar,
                 'base64',
-                '<img src="data:image/jpeg;base64,somedatastring" alt="Avatar"/>'
+                '<img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiByPSI1MCIgc3R5bGU9ImZpbGw6ICNlNWIzY2EiIC8+PHRleHQgeD0iNTAlIiB5PSI1NSUiIHN0eWxlPSJmaWxsOiAjODUyZTU1OyB0ZXh0LWFuY2hvcjogbWlkZGxlOyBkb21pbmFudC1iYXNlbGluZTogbWlkZGxlOyBmb250LXNpemU6IDUwcHgiPlROPC90ZXh0Pjwvc3ZnPg==" />'
             ]
         ];
     }
