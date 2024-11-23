6ada7fb4c669cc57e21892fa2d3648f5b64cc7b2
renfordt <jannikrenfordt@gmx.de>
2024-11-23 21:12:53 +0100

    Update text color in LarvatarTest assertions
    Adjusted the hexadecimal value for the fill property of the text color in the SVG output to better match design specifications. This change ensures the generated avatar's text color is correctly tested against the expected output.

--- tests/LarvatarTest.php
+++ tests/LarvatarTest.php
@@ -10,7 +10,7 @@ class LarvatarTest extends TestCase
     {
         $larvatar = new Larvatar(LarvatarTypes::InitialsAvatar, 'Test Name', 'test@example.com');
         $this->assertEquals(
-            '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100"><circle cx="50" cy="50" r="50" style="fill: #e5b3c9" /><text x="50%" y="55%" style="fill: #852e55; text-anchor: middle; dominant-baseline: middle; font-weight: normal; font-family: Segoe UI, Helvetica, sans-serif; font-size: 50px">TN</text></svg>',
+            '<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="100"><circle cx="50" cy="50" r="50" style="fill: #e5b3c9" /><text x="50%" y="55%" style="fill: #852d55; text-anchor: middle; dominant-baseline: middle; font-weight: normal; font-family: Segoe UI, Helvetica, sans-serif; font-size: 50px">TN</text></svg>',
             $larvatar->getImageHTML()
         );
     }
