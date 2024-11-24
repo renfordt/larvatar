fcf94adcd3a7834764d32d67a4597cdec58ebc9f
renfordt <jannikrenfordt@gmx.de>
2024-11-24 22:31:11 +0100

    Update avatar generation method in LarvatarTrait
    Refactored the `getAvatar` method to include additional parameters and updated its documentation to reflect the changes. The email is now optional, the size parameter has been added, and the encoding parameter now accepts a boolean to determine base64 encoding.

--- src/Traits/LarvatarTrait.php
+++ src/Traits/LarvatarTrait.php
@@ -8,13 +8,15 @@ use Renfordt\Larvatar\Larvatar;
 trait LarvatarTrait
 {
     /**
-     * Retrieves the avatar image HTML based on the provided name, email, avatar type, and encoding.
+     * Generates an avatar image based on the provided parameters.
      *
-     * @param  string  $name  The name used to generate the avatar.
-     * @param  string  $email  The email address used to generate the avatar.
-     * @param  LarvatarTypes  $type  The type of avatar to generate. Default is initial avatars.
-     * @param  string  $encoding  The encoding type for the avatar image. Default is empty string.
-     * @return string The HTML representation of the avatar image.
+     * @param string $name The name to be used for generating the avatar.
+     * @param string $email Optional email to be used for generating the avatar.
+     * @param int $size The size of the avatar image.
+     * @param LarvatarTypes $type The type of avatar to generate.
+     * @param bool $encoding Whether to encode the image in base64.
+     *
+     * @return string The generated avatar image in HTML format.
      */
     public function getAvatar(string $name, string $email = '', int $size = 100, LarvatarTypes $type = LarvatarTypes::InitialsAvatar, bool $encoding = false): string
     {
