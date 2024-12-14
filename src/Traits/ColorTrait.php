ddae469c83f2f3440e3c35d7b74811ca98265f51
renfordt <jannikrenfordt@gmx.de>
2024-12-14 23:04:50 +0100

    Simplify and correct docblock annotations.
    Updated the docblock formatting for better consistency and readability. Added type specificity to the return annotation to improve clarity and type safety.

--- src/Traits/ColorTrait.php
+++ src/Traits/ColorTrait.php
@@ -10,10 +10,10 @@ trait ColorTrait
     /**
      * Get the color set for a given name.
      *
-     * @param  Name  $name  The name to get the color set for.
-     * @param  float  $textLightness  The lightness value for the text color. Default is 0.35.
-     * @param  float  $backgroundLightness  The lightness value for the background color. Default is 0.8.
-     * @return array An array containing the dark and light colors in HSL format.
+     * @param Name $name The name to get the color set for.
+     * @param float $textLightness The lightness value for the text color. Default is 0.35.
+     * @param float $backgroundLightness The lightness value for the background color. Default is 0.8.
+     * @return array<HSLColor> An array containing the dark and light colors in HSL format.
      */
     public function getColorSet(Name $name, float $textLightness = 0.35, float $backgroundLightness = 0.8): array
     {
